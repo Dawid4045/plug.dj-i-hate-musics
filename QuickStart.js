4a3ea2ccb2af08939cf9e566086b541f62559b54
PolskiSlimak <slimak-75@wp.pl>
2020-06-14 11:46:53 +0200

    Update QuickStart.js

--- QuickStart.js
+++ QuickStart.js
@@ -6,30 +6,6 @@ var quickstart;
         }
         QuickStart.main = function (args) {
             console.info("No elo jestem twoim wisielcem");
-            \\ Ta metoda odpowiada za generowanie randomowych cyfr i ona nie dziala var generator = new java.util.Random();
-            var slowo = "Kamil";
-            \\ Ta metoda odpowiada za mozliwosc pobierania od uzytkownika cyfr/stringow var scaner = new java.util.Scanner(java.lang.System["in"]);
-            var literki = (slowo).split('');
-            for (var i = 0; i < slowo.length; i++) {
-                literki[i] = '*';
-            }
-            ;
-            console.info(literki);
-            var dziala = true;
-            while ((dziala)) {
-                console.info("Zgaduj!");
-                var wybor = scaner.next();
-                if ((function (o1, o2) { if (o1 && o1.equals) {
-                    return o1.equals(o2);
-                }
-                else {
-                    return o1 === o2;
-                } })(wybor, slowo)) {
-                    console.info("gj!");
-                    break;
-                }
-            }
-            ;
         };
         return QuickStart;
     }());
